0af7cd0f3068c83c93c4fdad3941080d92e662d0
Raman Gupta <ramanxg@gmail.com>
2020-01-11 03:34:16 -0800

    added firebase config

--- pages/api/createRide.js
+++ pages/api/createRide.js
@@ -1,4 +1,19 @@
+// // Firebase App (the core Firebase SDK) is always required and
+// // must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// // Add the Firebase products that you want to use
+// // require("firebase/auth");
+require("firebase/firestore");
+
+var database = firebase.firestore()
+
 async function createRide(req, res) {
+    database.collection("rides").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(`${doc.id} => ${doc.data()}`);
+        });
+    });
     res.end("Create Ride API!!");
 }
 
