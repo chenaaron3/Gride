3d2849332919a535de49d7658a9594bf1bedebda
iBessonob <bessonov3145@gmail.com>
2020-01-11 10:22:09 -0800

    Finished driverConfirm.js

--- pages/driverConfirm.js
+++ pages/driverConfirm.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import Head from 'next/head'
+import Head from 'next/head';
+import LandingPage from './index.js';
+import LabeledButton from "../public/Components/LabeledButton";
+import "../public/css/driverConfirm.scss";
 
 class DriverConfirm extends React.Component
 {
     render()
     {
         return (<React.Fragment>
-            <Head>
+            <head>
                 <title>Driver Confirmation</title>
                 <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <p>Driver Confirmation Page!</p>
+            </head>
+            <div class = "Background">
+                <div class="Inner">
+                    <div class="Header">
+                        <h1>Confirmed!</h1>
+                    </div>
+                    <div class="Message">
+                        <p>We'll send you a text with more information</p>
+                    </div>
+                    <div class="Home">
+                     <LabeledButton label="Finished?" txt="Go Home!" btnhref="/"/>
+                    </div>
+                </div>
+            </div>
+
         </React.Fragment>)
     }
 }
